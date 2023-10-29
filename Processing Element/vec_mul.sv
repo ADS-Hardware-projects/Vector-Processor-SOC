420cf797d5f23811ad5270ae7febbfa02cb4127b
Uvin99 <81609763+Uvin99@users.noreply.github.com>
2023-10-29 10:21:00 +0530

    fully combinational

--- Processing Element/vec_mul.sv
+++ Processing Element/vec_mul.sv
@@ -17,7 +17,7 @@ module vec_mul #(
   // Padding to a power of 2 
   localparam C_PAD = 2**$clog2(C);
 //logic signed [W_Y-1:0] adder_tree [DEPTH+1][C_PAD]; //  adder_tree
-  logic signed [C_PAD-1:0][W_Y-1:0]multiplied_elements ;
+ // logic signed [C_PAD-1:0][W_Y-1:0]multiplied_elements ;
   logic signed [W_Y-1:0] total;
   
   logic signed   [W_Y-1:0] y;
@@ -34,7 +34,7 @@ always_ff @(posedge clk)begin
     if (enable == 0) loop_counter <= 0;
     else begin 
     loop_counter <= loop_counter + 1'b1;
-    if (loop_counter !=0 && loop_counter == 2)begin 
+    if (loop_counter !=0 && loop_counter == 1)begin 
      v_valid <=1;
      end
     else begin v_valid <= 0;
@@ -44,17 +44,17 @@ end
 
 
 
-  for (c=0; c<C_PAD; c=c+1)
-      always_ff @(posedge clk)  // register after each mul
-         //if (enable)adder_tree[0][c] <= $signed(k_padded[c]) * $signed(x_padded[c]);
-         if (enable) multiplied_elements [c][W_Y-1:0] <= $signed(k_padded[c]) * $signed(x_padded[c]);
+//  for (c=0; c<C_PAD; c=c+1)
+//      always_ff @(posedge clk)  // register after each mul
+//         //if (enable)adder_tree[0][c] <= $signed(k_padded[c]) * $signed(x_padded[c]);
+//         if (enable) multiplied_elements [c][W_Y-1:0] <= $signed(k_padded[c]) * $signed(x_padded[c]);
   
  always_comb begin
  total =0;
  for (int n=0; n<C_PAD; n=n+1) begin
     
-    if (enable) total = total + multiplied_elements [n][W_Y-1:0];
-    
+    //if (enable) total = total + multiplied_elements [n][W_Y-1:0];
+    if (enable) total = total + $signed(k_padded[n]) * $signed(x_padded[n]);
     
     end
 end
